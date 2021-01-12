b73ef269c68354e7500b133bd3337c3357d81f1a
VincentBaron <vbaron@student.42.fr>
2021-01-12 20:59:32 +0100

    Sprite good only order missing

--- textures/sprites.c
+++ textures/sprites.c
@@ -6,7 +6,7 @@
 /*   By: vincentbaron <vincentbaron@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/06 16:37:04 by vbaron            #+#    #+#             */
-/*   Updated: 2021/01/12 20:56:09 by vincentbaro      ###   ########.fr       */
+/*   Updated: 2021/01/12 20:53:38 by vincentbaro      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,7 +65,7 @@ void    sort_sprites(t_general *mother)
 {
     int i;
 	int temp;
-
+    
     if(!(mother->sprite.sprite_order = (int *)malloc(sizeof(int) * mother->sprite.sprite_count)))
     {
         error();
@@ -80,10 +80,10 @@ void    sort_sprites(t_general *mother)
     while (i < mother->sprite.sprite_count)
     {
         mother->sprite.sprite_order[i] = i;
-        mother->sprite.sprite_distance[i] = ((mother->gps.pos.x - mother->sprite.elem[i].x) * (mother->gps.pos.x - mother->sprite.elem[i].x) + (mother->gps.pos.y - mother->sprite.elem[i].y) * (mother->gps.pos.y - mother->sprite.elem[i].y));
+        mother->sprite.sprite_distance[i] = ((mother->gps.pos.x - mother->sprite.elem[i].x) * (mother->gps.pos.x - mother->sprite.elem[i].x) + (mother->gps.pos.y - mother->sprite.elem[i].y) * (mother->gps.pos.y - mother->sprite.elem[i].y)); 
         i++;
-    }
-
+    }    
+    
     i = 0;
 	while (i + 1 < mother->sprite.sprite_count)
 	{
@@ -103,13 +103,13 @@ void    sort_sprites(t_general *mother)
 
 void    sprite_distance(t_general *mother, int i)
 {
-    mother->sprite.pos.x = mother->sprite.elem[i].x - mother->gps.pos.x + 0.5; // RAJOUTER + 0.5
-    mother->sprite.pos.y = mother->sprite.elem[i].y - mother->gps.pos.y + 0.5; // RAJOUTER + 0.5
-    mother->sprite.inv_det = 1.0 / (mother->gps.plane.x * mother->gps.dir.y - mother->gps.dir.x * mother->gps.plane.y); // PLANE Y A LA FIN
+    mother->sprite.pos.x = mother->sprite.elem[i].x - mother->gps.pos.x + 0.5;
+    mother->sprite.pos.y = mother->sprite.elem[i].y - mother->gps.pos.y + 0.5;
+    mother->sprite.inv_det = 1.0 / (mother->gps.plane.x * mother->gps.dir.y - mother->gps.dir.x * mother->gps.plane.y);
     mother->sprite.transform.x = mother->sprite.inv_det * (mother->gps.dir.y * mother->sprite.pos.x - mother->gps.dir.x * mother->sprite.pos.y);
     mother->sprite.transform.y = mother->sprite.inv_det * (-mother->gps.plane.y * mother->sprite.pos.x + mother->gps.plane.x * mother->sprite.pos.y);
     mother->sprite.spriteScreen = (int)((mother->args.R[0] / 2) * (1 + mother->sprite.transform.x / mother->sprite.transform.y));
-    mother->sprite.sprite_height = abs((int)(mother->args.R[1] / (mother->sprite.transform.y))); // UNE PARENTHESE RAJOUTÉ
+    mother->sprite.sprite_height = abs((int)(mother->args.R[1] / (mother->sprite.transform.y)));
     mother->sprite.sprite_width = abs((int)(mother->args.R[1] / (mother->sprite.transform.y)));
 
 }
@@ -137,9 +137,9 @@ static void    sprite_display(t_general *mother, int j, int stripe)
 
     d = j * 256 - mother->args.R[1] * 128 + mother->sprite.sprite_height * 128;
     txtr_y = ((d * mother->args.text[4].text_height) / mother->sprite.sprite_height) / 256;
-    mother->mlx.img_ray.color = *(unsigned int *)(mother->args.text[4].img_text.addr + mother->args.text[4].img_text.size_line * txtr_y + mother->sprite.tex.x * (mother->args.text[4].img_text.bpp / 8)); //RAJOUT DE PARENTHESE
-    if ((mother->mlx.img_ray.color & 0x00FFFFFF) != 0)
-        draw_pixel(&(mother->mlx.img_ray), stripe, j); // CHANGEMENT text_y en j
+    mother->mlx.img_ray.color = *(unsigned int *)(mother->args.text[4].img_text.addr + mother->args.text[4].img_text.size_line * txtr_y + mother->sprite.tex.x * mother->args.text[4].img_text.bpp / 8);
+    if ((mother->mlx.img_ray.color & 0x00FFFFFF) != 0) 
+        draw_pixel(&(mother->mlx.img_ray), stripe, j);
 }
 
 void    sprite_projection(t_general *mother)
@@ -147,7 +147,7 @@ void    sprite_projection(t_general *mother)
     int i;
     int j;
     int stripe;
-
+    
     i = 0;
     while (i < mother->sprite.sprite_count)
     {
@@ -156,7 +156,7 @@ void    sprite_projection(t_general *mother)
         stripe = mother->sprite.draw_start.x;
         while (stripe < mother->sprite.draw_end.x)
         {
-            mother->sprite.tex.x = (int)(256 * (stripe - (-mother->sprite.sprite_width / 2 + mother->sprite.spriteScreen)) * mother->args.text[4].text_width / mother->sprite.sprite_width) / 256; // SPRITE WIDTH (SECOND) MAUVAIS
+            mother->sprite.tex.x = (int)(256 * (stripe - (-mother->sprite.sprite_width / 2 + mother->sprite.spriteScreen)) * mother->sprite.sprite_width) / 256;
             if (mother->sprite.transform.y > 0 && stripe > 0 && stripe < mother->args.R[0] && mother->sprite.transform.y < mother->sprite.zbuffer[stripe])
             {
                 j = mother->sprite.draw_start.y;
