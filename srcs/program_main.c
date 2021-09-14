32202fcbb6bf28985a6114232b0fe489b3e09005
VincentBaron <vincentbaron1996@gmail.com>
2021-09-14 22:29:52 +0200

    norm up to program main

--- srcs/program_main.c
+++ srcs/program_main.c
@@ -6,52 +6,38 @@
 /*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/19 12:38:34 by vbaron            #+#    #+#             */
-/*   Updated: 2021/09/13 21:50:42 by user42           ###   ########.fr       */
+/*   Updated: 2021/09/14 22:29:05 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cub3d.h"
 
-void    free_program(t_general *mother)
+void	error(t_general *mother, int e)
 {
-    int i;
-
-    if (mother->args.matrix)
-    {
-        i = 0;
-        while(mother->args.matrix[i++])
-            ft_free(mother->args.matrix[i]);
-    }
-}
-
-void    error(t_general *mother, int e)
-{
-    (void)mother;
-    if (e == 1 || e == 4 || e == 6 || e == 8)
-    {
-        if (mother->args.line)
-            free(mother->args.line);
-        if (mother->args.map)
-            free(mother->args.map);
-        if (e == 1)
-            ft_putstr_fd("- Error 1: Map is unvalid -", 1);
-        if (e == 4)
-            ft_putstr_fd("- Error 4: Too many players -", 1);
-        if (e == 6)
-            ft_putstr_fd("- Error 6: Invalid texture -", 1);
-        if (e == 8)
-            ft_putstr_fd("- Error 8: Invalid file -", 1);
-    }
-    if (e == 2)
-        ft_putstr_fd("- Error 2: Arguments invalid -", 1);
-    if (e == 3)
-        ft_putstr_fd("- Error 3: Malloc Error -", 1);
-    if (e == 5)
-        ft_putstr_fd("- Error 5: Could not create bmp image -", 1);
-
-    if (e != 2)
-        ft_clean(mother);
-    exit(1);
+	if (e == 1 || e == 4 || e == 6 || e == 8)
+	{
+		if (mother->args.line)
+			free(mother->args.line);
+		if (mother->args.map)
+			free(mother->args.map);
+		if (e == 1)
+			ft_putstr_fd("- Error 1: Map is unvalid -", 1);
+		if (e == 4)
+			ft_putstr_fd("- Error 4: Too many players -", 1);
+		if (e == 6)
+			ft_putstr_fd("- Error 6: Invalid texture -", 1);
+		if (e == 8)
+			ft_putstr_fd("- Error 8: Invalid file -", 1);
+	}
+	if (e == 2)
+		ft_putstr_fd("- Error 2: Arguments invalid -", 1);
+	if (e == 3)
+		ft_putstr_fd("- Error 3: Malloc Error -", 1);
+	if (e == 5)
+		ft_putstr_fd("- Error 5: Could not create bmp image -", 1);
+	if (e != 2)
+		ft_clean(mother);
+	exit(1);
 }
 
 int	check_file(char *file)
@@ -67,11 +53,11 @@ int	check_file(char *file)
 	return (1);
 }
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-    t_general mother;
+	t_general	mother;
 
-    if (argc != 2)
+	if (argc != 2)
 		error(&mother, 2);
 	if (!check_file(argv[1]))
 		error(&mother, 2);
@@ -81,10 +67,10 @@ int main(int argc, char **argv)
 	mother.args.fd = open(argv[1], O_RDONLY);
 	if (mother.args.fd == -1)
 		error(&mother, 2);
-    init_vars(&mother);
-    map_parsing(&(mother.args), &mother);
-    init_map(&mother);
-    position_info(&mother);
-    game_start(&mother);
-    return (0);
+	init_vars(&mother);
+	map_parsing(&(mother.args), &mother);
+	init_map(&mother);
+	position_info(&mother);
+	game_start(&mother);
+	return (0);
 }
